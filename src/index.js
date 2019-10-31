ef58ae5f7437c104f5ff81f15d8bbf5e64f0f624
github username <github email address>
2019-10-31 13:07:44 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 let addToy = false
-
+const toysUrl = 'http://localhost:3000/toys'
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  const newToyForm = document.querySelector('.add-toy-form')
+  getToys()
+  // postToys()
+  
+  
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +17,99 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
+ 
+
+
+  function getToys(){
+    return fetch(toysUrl)
+    .then(resp => resp.json())
+    .then(toys => renderToys(toys))
+  }
+  
+  function renderToys(toys){
+    toys.forEach(function(toy){
+      renderToy(toy)
+
+    })
+  
+
+
+  }
+
+  function renderToy(toy){
+   let toyCollection = document.querySelector('#toy-collection')
+   let newDiv = document.createElement('div')
+   newDiv.className = 'card'
+   
+   let toyName = document.createElement('h2')
+    toyName.textContent = toy.name
+
+    let toyImg = document.createElement('img')
+     toyImg.src = toy.image
+     toyImg.className = 'toy-avatar'
+
+   let toyLikes = document.createElement('p')
+     toyLikes.innerHTML = toy.likes 
+     
+
+    let toyLikeButton = document.createElement('button')
+     toyLikeButton.innerHTML = "Like"
+     toyLikeButton.className = 'like-btn'
+
+    
+
+     
+     
+
+
+   toyCollection.append(newDiv)
+   newDiv.append(toyName,toyImg,toyLikes,toyLikeButton)
+
+  }
+
+ /// add new a new toy and persist to the server with fetch 
+
+    newToyForm.addEventListener('submit', function(e){
+      e.preventDefault()
+      let toyData = { 
+        "name": newToyForm.name.value,
+        "image":newToyForm.image.value,
+        "likes": 0
+     
+     }
+      postToys(toyData)
+      .then(function(toy){
+        renderToy(toy)
+        
+      })
+
+    })
+
+    function postToys(toyData){
+
+      
+      
+
+      let configurationObject = { 
+        method : "POST",
+        headers: { 
+          "Content-Type": "application/json",
+           "Accept": "application/json"
+        },
+        
+        body:JSON.stringify(toyData)
+  
+      }
+        
+
+     return fetch(toysUrl, configurationObject)
+      .then(function(response){
+        return response.json()
+      })
+    }
+
+    
+
+   
 
 })
