e866a28f820e32b8f799775668912edde97442dd
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-11-07 14:48:06 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,88 +13,106 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
   const baseURI = "http://localhost:3000/toys"
+  // const patchURI = "http://localhost:3000/toys/"
+  const showToy = document.querySelector("#toy-collection")
 
-  function fetchAndysToys() {
-    return fetch(baseURI)
-    .then(function(response){
-      return response.json()
-    })
-  }
-
-  function showAndysToys() {
+  function initialise() {
     fetchAndysToys()
-      .then(function(toys) {
-        for(let i = 0; i < toys.length; i ++) {
-          showToy(toys[i])
-        }
-      })
+    document.querySelector(".submit").addEventListener("click", addNewToy)
   }
 
-  function showToy(toy) {
-    const card = document.createElement("div")
-    card.classList.add("card")
-    card.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img src="${toy.image}" class="toy-avatar">
-    <p>${toy.likes}</p>
-    <button class="like-btn" id="${toy.id}">Like <3</button>
-    `
-    document.querySelector("#toy-collection").appendChild(card)
-    card.children[card.children.length - 1].addEventListener("click", increaseToyLikes)
+  function fetchAndysToys(toys) {
+    fetch(baseURI)
+    .then(toys => toys.json())
+    .then(toys => sortToys(toys))
   }
 
-  document.querySelector(".add-toy-form").addEventListener("submit", function(e){
+  function sortToys(toys) {
+    for(let i = 0; i < toys.length; i ++)
+    showAndysToys(toys[i])
+  }
+
+  function showAndysToys(toy) {
+    toyShower = document.createElement("div")
+    toyShower.id = toy.id
+    toyShower.classList.add("card")
+    toyName = document.createElement("h2")
+    toyName.textContent = toy.name
+    toyImage = document.createElement("img")
+    toyImage.classList.add("toy-avatar")
+    toyImage.src = toy.image
+    toyLikes = document.createElement("p")
+    toyLikes.textContent = toy.likes
+    likeButton = document.createElement("button")
+    likeButton.classList.add("like-btn")
+    likeButton.dataset.id = toy.id
+    likeButton.innerText = "Like <3"
+    likeButton.addEventListener("click", increaseLikes)
+    deleteButton = document.createElement("button")
+    deleteButton.classList.add("delete-btn")
+    deleteButton.innerText = "Give Toy to Sid"
+    deleteButton.dataset.id = toy.id
+    deleteButton.addEventListener("click", deleteToy)
+
+    toyShower.append(toyName, toyImage, toyLikes, likeButton, deleteButton)
+    showToy.append(toyShower)
+    console.log(toy)
+  }
+
+  function addNewToy(e) {
     e.preventDefault()
-    const newToy = {
-      name: e.target.children[1].value,
-      image: e.target.children[3].value,
-      likes: 0
+
+    const createToy = {
+      name : document.querySelector('input[name= "name"]').value ,
+      image : document.querySelector(`input[name= "image"]`).value ,
+      likes : 0
+      }
+      
+      const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      },
+      body: JSON.stringify(createToy)
     }
-    createToy(newToy)
-    .then(function(toy){
-      showToy(toy)
-    })
-})
-
-function createToy(newToy) {
-  const configurationObject = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(newToy)
+    return fetch(baseURI, configObject)
+    .then( res => res.json())
+    .then(toy => showAndysToys(toy))
   }
-  return fetch(baseURI, configurationObject)
-    .then(function(response){
-      return response.json()
-    })
-}
-function increaseToyLikes(e) {
-  const updatedToy = {
-    likes: parseInt(e.target.previousElementSibling.innerText) + 1
+
+  function increaseLikes(e) {
+    toyId = e.target.dataset.id
+    likes = { likes : parseInt(e.target.previousSibling.innerText) + 1 }
+    newLike(likes, toyId, e)
   }
-  updateToy(updatedToy, e.target.id)
-    .then(function(toy){
-      e.target.previousElementSibling.innerText = toy.likes
-    })
-}
 
-function updateToy(updatedToy, toyId) {
-  const configurationObject = {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(updatedToy)
+  function newLike(likes, toyId, e) {
+    const likeIncreaser = {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      },
+      body: JSON.stringify(likes)
+    }
+    return fetch(baseURI + "/" + `${toyId}`, likeIncreaser)
+    .then( res => res.json())
+    .then( toy => likedToy(toy, e))
   }
-  return fetch(`${baseURI}/${toyId}`, configurationObject)
-    .then(function(response){
-      return response.json()
-    })
-}
 
-  showAndysToys()
+  function likedToy(toy, e) {
+    e.target.previousSibling.innerText = toy.likes
+    // const card = document.querySelector(`div #${toy.id}`)
+    // debugger
+  }
 
+  function deleteToy(e) {
+    toyId = e.target.dataset.id
+    // debugger
+    return fetch(baseURI + "/" + `${toyId}`, {"method":"DELETE"})
+    .then(e.target.parentElement.remove())
+    }
+
+    initialise()
 })
